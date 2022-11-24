028b8323f00ffc9a973c4f65b34e3428db10f4fd
hiron <ivchpavel@gmail.com>
2022-11-24 17:35:19 +0300

    Add project files.

---
+++ BrokerUtils.cs
@@ -0,0 +1,36 @@
+﻿namespace TSLab.DataSource
+{
+    public static class BrokerUtils
+    {
+        public static string GetGroup(this Broker broker)
+        {
+            switch (broker)
+            {
+                case Broker.AlorDemo:
+                case Broker.AlorMicex:
+                case Broker.AlorForts:
+                case Broker.AlorDemoForts:
+                    return "Alor";
+                case Broker.Bittrex:
+                case Broker.Bitfinex:
+                case Broker.BinanceSpot:
+                case Broker.Deribit:
+                case Broker.Huobi:
+                case Broker.BinanceFutures:
+                case Broker.BinanceFutCoin:
+                case Broker.Bitmex:
+                case Broker.OkxSpot:
+                case Broker.OkxFutures:
+                case Broker.OkxPerpSwap:
+                case Broker.OkxMargin:
+                case Broker.ByBitSpot:
+                case Broker.ByBitPerpUsdt:
+                case Broker.ByBitPerpInverse:
+                case Broker.ByBitFutInverse:
+                    return "Bitcoin";
+                default:
+                    return broker.ToString();
+            }
+        }
+    }
+}
